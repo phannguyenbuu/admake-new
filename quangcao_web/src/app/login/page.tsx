6f6737248f27a18a21730b1978b56ff9a12405fe
phannguyenbuu <phannguyenbuu@gmail.com>
2025-11-05 22:06:06 +0700

    J16

--- quangcao_web/src/app/login/page.tsx
+++ quangcao_web/src/app/login/page.tsx
@@ -1,40 +1,35 @@
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import LoginForm from "./login.form";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 
 export const Component = () => {
   const navigate = useNavigate();
-  
-  const accessToken = sessionStorage.getItem("accessToken");
-  if (accessToken) {
-    // Nếu token tồn tại trong sessionStorage thì redirect dashboard tránh nhấp nháy
-    navigate("/dashboard", { replace: true });
-  } else {
-    sessionStorage.removeItem("accessToken"); // xóa nếu không thì có thể còn sót
-  }
+
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (accessToken) {
+      navigate("/dashboard", { replace: true });
+    } else {
+      sessionStorage.removeItem("accessToken");
+    }
+  }, [navigate]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm min-w-[400px]">
-        {/* logo */}
-        <div className="flex items-center justify-center pb-5">
-          <div className="flex items-center justify-center">
-            <img src="/logo.jpg" alt="logo" className="w-13 h-13" />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm min-w-[400px]">
+          {/* logo */}
+          <div className="flex items-center justify-center pb-5">
+            <div className="flex items-center justify-center">
+              <img src="/logo.jpg" alt="logo" className="w-13 h-13" />
+            </div>
+            <div className="flex items-center justify-center">
+              <img src="/ADMAKE.svg" alt="ADMAKE" className="h-8" />
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-            <img src="/ADMAKE.svg" alt="ADMAKE" className="h-8" />
-          </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
-    </div>
+    </Suspense>
   );
-};
-
-// Loader vẫn có thể dùng để redirect nếu cần
-export const loader = async () => {
-  if (sessionStorage.getItem("accessToken")) {
-    return redirect("/dashboard");
-  }
-  return null;
 };
